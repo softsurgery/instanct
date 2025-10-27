9a1d2805a1e9f31aa3a87cfc88f78c4a1310516e
softsurgery <109615722+softsurgery@users.noreply.github.com>
2025-10-27 11:58:57 +0100

    feat: add app title and favicon

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import type { AppProps } from "next/app";
 import Application from "@/components/Application";
+import Head from "next/head";
 import { appWithTranslation } from "next-i18next";
 import nextI18nextConfig from "../../next-i18next.config.mjs";
 import "@/styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Application Component={Component} pageProps={pageProps} />;
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Instanct Backoffice</title>
+        <meta name="description" content="Instanct Backoffice" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Application Component={Component} pageProps={pageProps} />
+    </React.Fragment>
+  );
 };
 
 export default appWithTranslation(App, nextI18nextConfig);
