e9b6507bcb994994d6ba03200dbe400a0565607c
softsurgery <109615722+softsurgery@users.noreply.github.com>
2025-11-12 11:00:04 +0100

    feat(reference-types): add generics to ref types & params

--- apps/web/src/types/reference-types.ts
+++ apps/web/src/types/reference-types.ts
@@ -1,6 +1,6 @@
 import { DatabaseEntity } from "./utils";
 
-export interface ResponseRefTypeDto extends DatabaseEntity {
+export interface ResponseRefTypeDto<T = object> extends DatabaseEntity {
   id: number;
   label: string;
   description: string;
@@ -8,34 +8,36 @@ export interface ResponseRefTypeDto extends DatabaseEntity {
   parentId?: number;
   parent?: ResponseRefTypeDto;
   children: ResponseRefTypeDto[];
-  extras: object;
+  extras: T;
 }
 
-export interface CreateRefTypeDto {
+export interface CreateRefTypeDto<T = object> {
   label: string;
   description: string;
   parentId?: number;
-  extras: object;
+  extras: T;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface UpdateRefTypeDto extends Partial<CreateRefTypeDto> {}
+export interface UpdateRefTypeDto<T = object>
+  extends Partial<CreateRefTypeDto<T>> {}
 
-export interface ResponseRefParamDto extends DatabaseEntity {
+export interface ResponseRefParamDto<T = object> extends DatabaseEntity {
   id: number;
   label: string;
   description: string;
   refTypeId: number;
   refType: ResponseRefTypeDto;
-  extras: object;
+  extras: T;
 }
 
-export interface CreateRefParamDto {
+export interface CreateRefParamDto<T = object> {
   label: string;
   description: string;
   refTypeId?: number;
-  extras: object;
+  extras: T;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface UpdateRefParamDto extends Partial<CreateRefParamDto> {}
+export interface UpdateRefParamDto<T = object>
+  extends Partial<CreateRefParamDto<T>> {}
