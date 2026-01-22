2b88235ecbf7334a3d5854571639f2d8e666ae1b
falcao <amine.saguer1@gmail.com>
2026-01-22 16:20:01 +0100

    fix(experience): add missing `id` to the response DTO

--- apps/api/src/modules/users/dtos/experience/response-experience.dto.ts
+++ apps/api/src/modules/users/dtos/experience/response-experience.dto.ts
@@ -6,6 +6,10 @@ import { ResponseUserDto } from '../user/response-user.dto';
 export class ResponseExperienceDto extends ResponseDtoHelper {
   @ApiProperty({ type: String })
   @Expose()
+  id: string;
+
+  @ApiProperty({ type: String })
+  @Expose()
   title?: string;
 
   @ApiProperty({ type: Date })
