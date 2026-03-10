c206a4c4d81ed42680db31ee946a0accbee943b4
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2026-03-10 01:33:36 +0000

    feat(auth): add validation decorators for usernameOrEmail and password in RequestSignInDto

--- apps/api/src/shared/auth/dtos/web/request-signin.dto.ts
+++ apps/api/src/shared/auth/dtos/web/request-signin.dto.ts
@@ -1,13 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { faker } from '@faker-js/faker';
+import { IsString, Length } from 'class-validator';
 
 export class RequestSignInDto {
   @ApiProperty({
     type: String,
     example: faker.internet.email(),
   })
+  @IsString()
   usernameOrEmail: string;
 
   @ApiProperty({ type: String, example: 'password123' })
+  @IsString()
+  @Length(8, 128)
   password: string;
 }
