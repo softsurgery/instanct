c23c9270768f403d2ddeff04cc8a18075bd0f904
softsurgery <housssemeddine.chibouni@fsb.ucar.tn>
2026-05-10 22:10:33 +0000

    feat(chat): use AbstractCrudRepo in message service

--- apps/api/src/shared/chat/services/message.service.ts
+++ apps/api/src/shared/chat/services/message.service.ts
@@ -1,91 +1,19 @@
 import { Transactional } from '@nestjs-cls/transactional';
-import { Injectable } from '@nestjs/common';
-import { FindManyOptions, FindOneOptions } from 'typeorm';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { DeepPartial, FindManyOptions } from 'typeorm';
 import { IQueryObject } from 'src/shared/database/interfaces/database-query-options.interface';
 import { QueryBuilder } from 'src/shared/database/utils/database-query-builder';
 import { PageDto } from 'src/shared/database/dtos/database.page.dto';
 import { PageMetaDto } from 'src/shared/database/dtos/database.page-meta.dto';
 import { MessageRepository } from '../repositories/message.repository';
 import { MessageEntity } from '../entities/message.entity';
-import { MessageNotFoundException } from '../errors/message/message.notfound.error';
-import { CreateMessageDto } from '../dtos/message/create-message.dto';
+import { AbstractCrudService } from 'src/shared/database/services/abstract-crud.service';
 
 @Injectable()
-export class MessageService {
-  constructor(private readonly messageRepository: MessageRepository) {}
-
-  async findOneById(id: number): Promise<MessageEntity> {
-    const message = await this.messageRepository.findOneById(id);
-    if (!message) throw new MessageNotFoundException();
-    return message;
+export class MessageService extends AbstractCrudService<MessageEntity> {
+  constructor(private readonly messageRepository: MessageRepository) {
+    super(messageRepository);
   }
-
-  async findOneByCondition(
-    query: IQueryObject = {},
-  ): Promise<MessageEntity | null> {
-    const queryBuilder = new QueryBuilder(this.messageRepository.getMetadata());
-    const queryOptions = queryBuilder.build(query);
-    return await this.messageRepository.findOne(
-      queryOptions as FindOneOptions<MessageEntity>,
-    );
-  }
-
-  async findAll(query: IQueryObject): Promise<MessageEntity[]> {
-    const queryBuilder = new QueryBuilder(this.messageRepository.getMetadata());
-    const queryOptions = queryBuilder.build(query);
-    return await this.messageRepository.findAll(
-      queryOptions as FindManyOptions<MessageEntity>,
-    );
-  }
-
-  async findAllPaginated(query: IQueryObject): Promise<PageDto<MessageEntity>> {
-    const queryBuilder = new QueryBuilder(this.messageRepository.getMetadata());
-    const queryOptions = queryBuilder.build(query);
-
-    const count = await this.messageRepository.getTotalCount({
-      where: queryOptions.where,
-    });
-    const entities = await this.messageRepository.findAll(
-      queryOptions as FindManyOptions<MessageEntity>,
-    );
-
-    const pageMetaDto = new PageMetaDto({
-      pageOptionsDto: {
-        page: Number(query.page),
-        take: Number(query.limit),
-      },
-      itemCount: count,
-    });
-
-    return new PageDto(entities, pageMetaDto);
-  }
-
-  @Transactional()
-  async save(
-    createMessageDto: Partial<MessageEntity>,
-    userId?: string,
-  ): Promise<MessageEntity> {
-    return await this.messageRepository.save({ ...createMessageDto, userId });
-  }
-
-  @Transactional()
-  async saveMany(
-    createMessageDto: CreateMessageDto[],
-  ): Promise<MessageEntity[]> {
-    return Promise.all(createMessageDto.map((dto) => this.save(dto)));
-  }
-
-  async softDelete(id: number): Promise<MessageEntity | null> {
-    return this.messageRepository.softDelete(id);
-  }
-
-  async delete(id: number): Promise<MessageEntity | null> {
-    const message = await this.messageRepository.findOneById(id);
-    if (!message) throw new MessageNotFoundException();
-    return this.messageRepository.remove(message);
-  }
-
-  //Extended methods ******************************************************************************
 
   async findPaginatedConversationMessages(
     query: IQueryObject,
@@ -132,5 +60,20 @@ export class MessageService {
     });
 
     return messages.length > 0 ? messages[0] : null;
+  }
+
+  @Transactional()
+  async createMessage(
+    createMessage: DeepPartial<MessageEntity>,
+    userId?: string,
+  ): Promise<MessageEntity> {
+    if (!userId) {
+      throw new BadRequestException('User id is required');
+    }
+    if (!createMessage.conversationId) {
+      throw new BadRequestException('Conversation id is required');
+    }
+
+    return this.save({ ...createMessage, userId });
   }
 }
