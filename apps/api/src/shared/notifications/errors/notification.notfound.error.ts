d4ed5b4ef69a9e538636751742b8787a9af19d7a
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2026-08-23 11:21:25 +0000

    ref: migrate NotificationService to AbstractCrudService and remove redundant exception handling

--- apps/api/src/shared/notifications/errors/notification.notfound.error.ts
+++
@@ -1,7 +0,0 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
-
-export class NotificationNotFoundException extends HttpException {
-  constructor() {
-    super('Notification not found', HttpStatus.NOT_FOUND);
-  }
-}
