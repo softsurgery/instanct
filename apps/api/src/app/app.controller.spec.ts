d1e19350d23d0df52ba5ccc2432045f656707968
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2026-09-21 15:35:51 +0000

    fix(api): update TypeScript paths and enhance health check tests

--- apps/api/src/app/app.controller.spec.ts
+++ apps/api/src/app/app.controller.spec.ts
@@ -14,9 +14,12 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('health', () => {
+    it('should return ok', () => {
+      expect(appController.getHealth()).toEqual({
+        message: 'ok',
+        timestamp: expect.any(String),
+      });
     });
   });
 });
